4e269d71222d141a5fc589b5f145b94286a69b4e
cagatay.civici <cagatay.civici@fd9223c1-c77d-41e1-af93-17165896985c>
2016-09-20 15:26:15 +0000

    add chart to dashboard

--- app/demo/view/dashboarddemo.ts
+++ app/demo/view/dashboarddemo.ts
@@ -39,13 +39,13 @@ export class DashboardDemo implements OnInit {
                     label: 'First Dataset',
                     data: [65, 59, 80, 81, 56, 55, 40],
                     fill: false,
-                    borderColor: '#4bc0c0'
+                    borderColor: '#FFC107'
                 },
                 {
                     label: 'Second Dataset',
                     data: [28, 48, 40, 19, 86, 27, 90],
                     fill: false,
-                    borderColor: '#565656'
+                    borderColor: '#03A9F4'
                 }
             ]
         }
