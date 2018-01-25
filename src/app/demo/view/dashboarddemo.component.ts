35dd97807b78242bddc9b2732f52e87dc5acec1b
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2018-01-25 06:52:19 +0000

    Menus Fixes

--- src/app/demo/view/dashboarddemo.component.ts
+++ src/app/demo/view/dashboarddemo.component.ts
@@ -22,6 +22,8 @@ export class DashboardDemoComponent implements OnInit {
 
     selectedCity: any;
 
+    selectedCar: Car;
+
     constructor(private carService: CarService, private eventService: EventService, private breadcrumbService: BreadcrumbService) {
       this.breadcrumbService.setItems([
         {label: ''},
