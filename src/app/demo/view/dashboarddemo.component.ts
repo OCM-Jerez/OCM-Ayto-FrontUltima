df1c691f14dd012f209355591ab06abdc834622e
onursenture <woof.uyelik@gmail.com>
2020-12-03 18:57:15 +0300

    Rollback chartData to resolve NG issue on server start

--- src/app/demo/view/dashboarddemo.component.ts
+++ src/app/demo/view/dashboarddemo.component.ts
@@ -64,24 +64,6 @@ export class DashboardDemoComponent implements OnInit {
             ]
         };
 
-        this.chartData2 = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-                {
-                    label: 'My First dataset',
-                    backgroundColor: '#80DEEA',
-                    borderColor: '#1E88E5',
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                },
-                {
-                    label: 'My Second dataset',
-                    backgroundColor: '#0097A7',
-                    borderColor: '#7CB342',
-                    data: [28, 48, 40, 19, 86, 27, 90]
-                }
-            ]
-        };
-
         this.scheduleOptions = {
             plugins: [ dayGridPlugin, timeGridPlugin, interactionPlugin ],
             defaultDate: '2017-02-12',
