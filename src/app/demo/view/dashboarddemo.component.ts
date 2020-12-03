acb359ad2bd82c4063bf825fac2d7670cd72fe51
onursenture <woof.uyelik@gmail.com>
2020-12-03 14:23:07 +0300

    Refactor timeline

--- src/app/demo/view/dashboarddemo.component.ts
+++ src/app/demo/view/dashboarddemo.component.ts
@@ -75,10 +75,10 @@ export class DashboardDemoComponent implements OnInit {
         };
 
         this.timelineEvents = [
-            {status: 'Ordered', date: '15/10/2020 10:30', icon: "pi pi-cog", color: '#9C27B0'},
-            {status: 'Processing', date: '15/10/2020 14:00', icon: "pi pi-cog", color: '#673AB7'},
-            {status: 'Shipped', date: '15/10/2020 16:15', icon: "pi pi-cog", color: '#FF9800'},
-            {status: 'Delivered', date: '16/10/2020 10:00', icon: "pi pi-cog", color: '#607D8B'}
+            {status: 'Ordered', date: '15/10/2020 10:30', icon: "pi pi-shopping-cart", color: '#E91E63'},
+            {status: 'Processing', date: '15/10/2020 14:00', icon: "pi pi-cog", color: '#FB8C00'},
+            {status: 'Shipped', date: '15/10/2020 16:15', icon: "pi pi-compass", color: '#673AB7'},
+            {status: 'Delivered', date: '16/10/2020 10:00', icon: "pi pi-check-square", color: '#0097A7'}
         ];
     }
 }
