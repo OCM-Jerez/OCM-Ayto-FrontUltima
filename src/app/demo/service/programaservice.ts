3a16e26fc0c32507de8bdbcbf9b3a4d1d8e7e36e
Miguel Angel Martinez <correomam@gmail.com>
2021-05-13 09:42:46 +0200

    Añado uso de catchError.

--- src/app/demo/service/programaservice.ts
+++ src/app/demo/service/programaservice.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse  } from '@angular/common/http';
+
+import { throwError } from 'rxjs';
+import { map, catchError  } from 'rxjs/operators';
 
 import { Programa } from '../domain/programa';
 
@@ -9,17 +11,17 @@ export class ProgramaService {
     codPro:string;
     desPro:string
 
-    constructor(private http: HttpClient) { }
+    constructor(private httpClient: HttpClient) { }
 
     getProductsSmall() {
-        return this.http.get<any>('assets/demo/data/products-small.json')
+        return this.httpClient.get<any>('assets/demo/data/products-small.json')
         .toPromise()
         .then(res => res.data as Programa[])
         .then(data => data);
     }
 
     getProducts() {
-        return this.http.get<any>('assets/demo/data/products.json')
+        return this.httpClient.get<any>('assets/demo/data/products.json')
         // tpPromise esta deprecado.
         //https://indepth.dev/posts/1287/rxjs-heads-up-topromise-is-being-deprecated
         .toPromise()
@@ -28,7 +30,7 @@ export class ProgramaService {
     }
 
     // async getProgramas() {
-    //     return await this.http.get<Programa[]>('http://localhost:3000/api/v1/programas')
+    //     return await this.httpClient.get<Programa[]>('http://localhost:3000/api/v1/programas')
     // tpPromise esta deprecado.
         //https://indepth.dev/posts/1287/rxjs-heads-up-topromise-is-being-deprecated
     //     .toPromise()
@@ -38,24 +40,32 @@ export class ProgramaService {
     //     // .then(data => data);
     // }
 
+   // https://platzi.com/clases/1731-angular-profesional/23607-manejo-de-errores/
+   // https://www.udemy.com/course/rxjs-de-cero-hasta-los-detalles/learn/lecture/16623364#questions/14456716
     getProgramas() {
-        return this.http.get<Programa[]>('http://localhost:3000/api/v1/programas')
+        return this.httpClient.get<Programa[]>('http://localhost:3000/api/v1/programas')
+        .pipe(
+            catchError(this.handleError),
+          );
            }
 
     getProductsMixed() {
-        return this.http.get<any>('assets/demo/data/products-mixed.json')
+        return this.httpClient.get<any>('assets/demo/data/products-mixed.json')
         .toPromise()
         .then(res => res.data as Programa[])
         .then(data => data);
     }
 
     getProductsWithOrdersSmall() {
-        return this.http.get<any>('assets/demo/data/products-orders-small.json')
+        return this.httpClient.get<any>('assets/demo/data/products-orders-small.json')
         .toPromise()
         .then(res => res.data as Programa[])
         .then(data => data);
     }
 
-
+    private handleError(error: HttpErrorResponse) {
+        console.log(error.message);
+        return throwError('ups algo salio mal');
+      }
 
 }
