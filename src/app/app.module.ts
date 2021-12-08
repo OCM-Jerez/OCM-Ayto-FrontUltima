eb6b0e3a1c021f12c3128b1002a55b251ff73931
mamjerez <correomam@gmail.com>
2021-12-08 04:34:19 +0100

    Remove folder utilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -97,35 +97,7 @@ import { AppRightMenuComponent } from "./layout/rightmenu/app.rightmenu.componen
 import { AppBreadcrumbComponent } from "./app.breadcrumb.component";
 import { AppTopBarComponent } from "./layout/topbar/app.topbar.component";
 import { AppFooterComponent } from "./layout/footer/app.footer.component";
-// import { DashboardComponent } from "./demo/view/dashboard.component";
-// import { DashboardAnalyticsComponent } from "./demo/view/dashboardanalytics.component";
-// import { FormLayoutDemoComponent } from "./demo/view/formlayoutdemo.component";
-// import { FloatLabelDemoComponent } from "./demo/view/floatlabeldemo.component";
-// import { InvalidStateDemoComponent } from "./demo/view/invalidstatedemo.component";
-// import { InputDemoComponent } from "./demo/view/inputdemo.component";
-// import { ButtonDemoComponent } from "./demo/view/buttondemo.component";
-// import { TableDemoComponent } from "./demo/view/tabledemo.component";
-// import { ListDemoComponent } from "./demo/view/listdemo.component";
-// import { TreeDemoComponent } from "./demo/view/treedemo.component";
-// import { PanelsDemoComponent } from "./demo/view/panelsdemo.component";
-// import { OverlaysDemoComponent } from "./demo/view/overlaysdemo.component";
-// import { MediaDemoComponent } from "./demo/view/mediademo.component";
-// import { MenusDemoComponent } from "./demo/view/menusdemo.component";
-// import { MessagesDemoComponent } from "./demo/view/messagesdemo.component";
-// import { MiscDemoComponent } from "./demo/view/miscdemo.component";
-// import { EmptyDemoComponent } from "./demo/view/emptydemo.component";
-// import { ChartsDemoComponent } from "./demo/view/chartsdemo.component";
-// import { FileDemoComponent } from "./demo/view/filedemo.component";
-// import { DocumentationComponent } from "./demo/view/documentation.component";
-import { DisplayComponent } from "./utilities/display.component";
-import { ElevationComponent } from "./utilities/elevation.component";
-import { FlexboxComponent } from "./utilities/flexbox.component";
-import { GridComponent } from "./utilities/grid.component";
-import { IconsComponent } from "./utilities/icons.component";
-import { WidgetsComponent } from "./utilities/widgets.component";
-import { SpacingComponent } from "./utilities/spacing.component";
-import { TypographyComponent } from "./utilities/typography.component";
-import { TextComponent } from "./utilities/text.component";
+
 import { AppProgramasComponent } from "./pages/programa/app.programas.component";
 import { AppCalendarComponent } from "./pages/plantilla/app.calendar.component";
 import { AppInvoiceComponent } from "./pages/plantilla/app.invoice.component";
@@ -272,8 +244,6 @@ import { CuentaGeneralComponent } from './pages/cuenta-general/cuenta-general.co
 
     ],
     declarations: [
-        // ButtonDemoComponent,
-        // ChartsDemoComponent,
         AppAccessdeniedComponent,
         AppBreadcrumbComponent,
         AppCalendarComponent,
@@ -298,33 +268,8 @@ import { CuentaGeneralComponent } from './pages/cuenta-general/cuenta-general.co
         DashboardAnalyticsComponent,
         DashboardComponent,
         DashboardtiemposComponent,
-        DisplayComponent,
-        // DocumentationComponent,
-        ElevationComponent,
-        // EmptyDemoComponent,
-        // FileDemoComponent,
-        FlexboxComponent,
-        // FloatLabelDemoComponent,
-        // FormLayoutDemoComponent,
-        GridComponent,
-        IconsComponent,
-        // InputDemoComponent,
-        // InvalidStateDemoComponent,
-        // ListDemoComponent,
-        // MediaDemoComponent,
-        // MenusDemoComponent,
-        // MessagesDemoComponent,
-        // MessagesDemoComponent,
-        // MiscDemoComponent,
-        // OverlaysDemoComponent,
-        // PanelsDemoComponent,
+
         ProgramaDetailComponent,
-        SpacingComponent,
-        // TableDemoComponent,
-        TextComponent,
-        // TreeDemoComponent,
-        TypographyComponent,
-        WidgetsComponent,
         EcoGastosComponent,
         OrganicosComponent,
         EcoIngresosComponent,
