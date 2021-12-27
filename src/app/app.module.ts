18b7743bf622107948f7c358cb9301e1e17650ba
mamjerez <correomam@gmail.com>
2021-12-27 17:01:47 +0100

    Comment innecesary code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { AppConfigComponent } from "./app.config.component";
 import { AppMainComponent } from "./app.main.component";
 import { AppMenuitemComponent } from "./app.menuitem.component";
 
+// import { AppCodeModule } from "./app.code.component";
+// import { AppInlineMenuComponent } from "./app.inlinemenu.component";
+
 import { HighchartsChartModule } from "highcharts-angular";
 
 import { AppLoginComponent } from "./auth/login/app.login.component";
@@ -26,30 +29,6 @@ import { AppRightMenuComponent } from "./layout/rightmenu/app.rightmenu.componen
 import { AppTopBarComponent } from "./layout/topbar/app.topbar.component";
 import { MenuService } from "./layout/menu/app.menu.service";
 
-// import { ProgramaService } from "./service/programaservice";
-import { BarrioService } from "./service/barrio.service";
-import { CapituloGastoService } from "./service/capitulo-gasto.service";
-import { CapituloIngresoService } from "./service/capitulo-ingreso.service";
-import { DelegacionService } from "./service/delegacion.service";
-import { DistritoService } from "./service/distrito.service";
-import { EcoGastoService } from "./service/ecogastoservice";
-import { EcoIngresoService } from "./service/ecoingresoservice";
-import { EnteService } from "./service/ente.service";
-import { OrganicoService } from "./service/organicoservice";
-import { OrganoContratacionService } from "./service/organo-contratacion.service";
-import { SeccionCensalService } from "./service/seccion-censal.service";
-import { SindicatoService } from "./service/sindicato.service";
-
-import { CountryService } from "./service/countryservice";
-import { CustomerService } from "./service/customerservice";
-import { EventService } from "./service/eventservice";
-import { IconService } from "./service/iconservice";
-import { NodeService } from "./service/nodeservice";
-import { PhotoService } from "./service/photoservice";
-import { ProductService } from "./service/productservice";
-
-// import { AppCodeModule } from "./app.code.component";
-// import { AppInlineMenuComponent } from "./app.inlinemenu.component";
 import { AppErrorComponent } from "./pages/error/app.error.component";
 import { AppNotfoundComponent } from "./pages/404notFound/app.notfound.component";
 import { AppProgramasComponent } from "./pages/programa/app.programas.component";
@@ -70,87 +49,111 @@ import { OrganoContratacionComponent } from './pages/organo-contratacion/organo-
 import { ProgramaDetailComponent } from "./pages/programa-detail/programa-detail.component";
 import { SeccionCensalComponent } from './pages/seccion-censal/seccion-censal.component';
 import { SindicatoComponent } from './pages/sindicato/sindicato.component';
+import { ToolbarModule } from "primeng/toolbar";
 
-import { AccordionModule } from "primeng/accordion";
-import { AutoCompleteModule } from "primeng/autocomplete";
-import { AvatarGroupModule } from "primeng/avatargroup";
-import { AvatarModule } from "primeng/avatar";
-import { BadgeModule } from "primeng/badge";
 import { BreadcrumbModule } from "primeng/breadcrumb";
-import { ButtonModule } from "primeng/button";
-import { CalendarModule } from "primeng/calendar";
-import { CardModule } from "primeng/card";
-import { CarouselModule } from "primeng/carousel";
-import { CascadeSelectModule } from "primeng/cascadeselect";
 import { ChartModule } from "primeng/chart";
-import { CheckboxModule } from "primeng/checkbox";
-import { ChipModule } from "primeng/chip";
-import { ChipsModule } from "primeng/chips";
-import { CodeHighlighterModule } from "primeng/codehighlighter";
-import { ColorPickerModule } from "primeng/colorpicker";
 import { ConfirmationService, MessageService } from "primeng/api";
 import { ConfirmDialogModule } from "primeng/confirmdialog";
 import { ConfirmPopupModule } from "primeng/confirmpopup";
-import { ContextMenuModule } from "primeng/contextmenu";
-import { DataViewModule } from "primeng/dataview";
-import { DialogModule } from "primeng/dialog";
-import { DividerModule } from "primeng/divider";
-import { DropdownModule } from "primeng/dropdown";
-import { FieldsetModule } from "primeng/fieldset";
-import { FileUploadModule } from "primeng/fileupload";
-import { FullCalendarModule } from "primeng/fullcalendar";
-import { GalleriaModule } from "primeng/galleria";
-import { InplaceModule } from "primeng/inplace";
-import { InputMaskModule } from "primeng/inputmask";
-import { InputNumberModule } from "primeng/inputnumber";
-import { InputSwitchModule } from "primeng/inputswitch";
 import { InputTextareaModule } from "primeng/inputtextarea";
 import { InputTextModule } from "primeng/inputtext";
-import { KnobModule } from "primeng/knob";
-import { LightboxModule } from "primeng/lightbox";
-import { ListboxModule } from "primeng/listbox";
-import { MegaMenuModule } from "primeng/megamenu";
+import { DialogModule } from "primeng/dialog";
 import { MenubarModule } from "primeng/menubar";
 import { MenuModule } from "primeng/menu";
-import { MessageModule } from "primeng/message";
-import { MessagesModule } from "primeng/messages";
-import { MultiSelectModule } from "primeng/multiselect";
-import { OrderListModule } from "primeng/orderlist";
-import { OrganizationChartModule } from "primeng/organizationchart";
-import { OverlayPanelModule } from "primeng/overlaypanel";
-import { PaginatorModule } from "primeng/paginator";
-import { PanelMenuModule } from "primeng/panelmenu";
-import { PanelModule } from "primeng/panel";
-import { PasswordModule } from "primeng/password";
-import { PickListModule } from "primeng/picklist";
-import { ProgressBarModule } from "primeng/progressbar";
-import { RadioButtonModule } from "primeng/radiobutton";
-import { RatingModule } from "primeng/rating";
-import { RippleModule } from "primeng/ripple";
-import { ScrollPanelModule } from "primeng/scrollpanel";
-import { ScrollTopModule } from "primeng/scrolltop";
-import { SelectButtonModule } from "primeng/selectbutton";
 import { SidebarModule } from "primeng/sidebar";
-import { SkeletonModule } from "primeng/skeleton";
-import { SlideMenuModule } from "primeng/slidemenu";
-import { SliderModule } from "primeng/slider";
-import { SplitButtonModule } from "primeng/splitbutton";
-import { SplitterModule } from "primeng/splitter";
-import { StepsModule } from "primeng/steps";
 import { TableModule } from "primeng/table";
 import { TabMenuModule } from "primeng/tabmenu";
 import { TabViewModule } from "primeng/tabview";
-import { TagModule } from "primeng/tag";
-import { TerminalModule } from "primeng/terminal";
-import { TieredMenuModule } from "primeng/tieredmenu";
 import { TimelineModule } from "primeng/timeline";
 import { ToastModule } from "primeng/toast";
-import { ToggleButtonModule } from "primeng/togglebutton";
-import { ToolbarModule } from "primeng/toolbar";
-import { TooltipModule } from "primeng/tooltip";
-import { TreeModule } from "primeng/tree";
-import { TreeTableModule } from "primeng/treetable";
-import { VirtualScrollerModule } from "primeng/virtualscroller";
+
+// import { AccordionModule } from "primeng/accordion";
+// import { AutoCompleteModule } from "primeng/autocomplete";
+// import { AvatarGroupModule } from "primeng/avatargroup";
+// import { AvatarModule } from "primeng/avatar";
+// import { BadgeModule } from "primeng/badge";
+// import { ButtonModule } from "primeng/button";
+// import { CalendarModule } from "primeng/calendar";
+// import { CardModule } from "primeng/card";
+// import { CarouselModule } from "primeng/carousel";
+// import { CascadeSelectModule } from "primeng/cascadeselect";
+// import { CheckboxModule } from "primeng/checkbox";
+// import { ChipModule } from "primeng/chip";
+// import { ChipsModule } from "primeng/chips";
+// import { CodeHighlighterModule } from "primeng/codehighlighter";
+// import { ColorPickerModule } from "primeng/colorpicker";
+// import { ContextMenuModule } from "primeng/contextmenu";
+// import { DataViewModule } from "primeng/dataview";
+// import { DividerModule } from "primeng/divider";
+// import { DropdownModule } from "primeng/dropdown";
+// import { FieldsetModule } from "primeng/fieldset";
+// import { FileUploadModule } from "primeng/fileupload";
+// import { FullCalendarModule } from "primeng/fullcalendar";
+// import { GalleriaModule } from "primeng/galleria";
+// import { InplaceModule } from "primeng/inplace";
+// import { InputMaskModule } from "primeng/inputmask";
+// import { InputNumberModule } from "primeng/inputnumber";
+// import { InputSwitchModule } from "primeng/inputswitch";
+// import { KnobModule } from "primeng/knob";
+// import { LightboxModule } from "primeng/lightbox";
+// import { ListboxModule } from "primeng/listbox";
+// import { MegaMenuModule } from "primeng/megamenu";
+// import { MessageModule } from "primeng/message";
+// import { MessagesModule } from "primeng/messages";
+// import { MultiSelectModule } from "primeng/multiselect";
+// import { OrderListModule } from "primeng/orderlist";
+// import { OrganizationChartModule } from "primeng/organizationchart";
+// import { OverlayPanelModule } from "primeng/overlaypanel";
+// import { PaginatorModule } from "primeng/paginator";
+// import { PanelMenuModule } from "primeng/panelmenu";
+// import { PanelModule } from "primeng/panel";
+// import { PasswordModule } from "primeng/password";
+// import { PickListModule } from "primeng/picklist";
+// import { ProgressBarModule } from "primeng/progressbar";
+// import { RadioButtonModule } from "primeng/radiobutton";
+// import { RatingModule } from "primeng/rating";
+// import { RippleModule } from "primeng/ripple";
+// import { ScrollPanelModule } from "primeng/scrollpanel";
+// import { ScrollTopModule } from "primeng/scrolltop";
+// import { SelectButtonModule } from "primeng/selectbutton";
+// import { SkeletonModule } from "primeng/skeleton";
+// import { SlideMenuModule } from "primeng/slidemenu";
+// import { SliderModule } from "primeng/slider";
+// import { SplitButtonModule } from "primeng/splitbutton";
+// import { SplitterModule } from "primeng/splitter";
+// import { StepsModule } from "primeng/steps";
+// import { TagModule } from "primeng/tag";
+// import { TerminalModule } from "primeng/terminal";
+// import { TieredMenuModule } from "primeng/tieredmenu";
+// import { ToggleButtonModule } from "primeng/togglebutton";
+// import { TooltipModule } from "primeng/tooltip";
+// import { TreeModule } from "primeng/tree";
+// import { TreeTableModule } from "primeng/treetable";
+// import { VirtualScrollerModule } from "primeng/virtualscroller";
+
+// import { ProgramaService } from "./service/programaservice";
+import { BarrioService } from "./service/barrio.service";
+import { CapituloGastoService } from "./service/capitulo-gasto.service";
+import { CapituloIngresoService } from "./service/capitulo-ingreso.service";
+import { DelegacionService } from "./service/delegacion.service";
+import { DistritoService } from "./service/distrito.service";
+import { EcoGastoService } from "./service/ecogastoservice";
+import { EcoIngresoService } from "./service/ecoingresoservice";
+import { EnteService } from "./service/ente.service";
+import { OrganicoService } from "./service/organicoservice";
+import { OrganoContratacionService } from "./service/organo-contratacion.service";
+import { SeccionCensalService } from "./service/seccion-censal.service";
+import { SindicatoService } from "./service/sindicato.service";
+import { CustomerService } from "./service/customerservice";
+import { EventService } from "./service/eventservice";
+import { IconService } from "./service/iconservice";
+
+import { ProductService } from "./service/productservice";
+
+// import { CountryService } from "./service/countryservice";
+// import { NodeService } from "./service/nodeservice";
+// import { PhotoService } from "./service/photoservice";
 
 @NgModule({
     imports: [
@@ -180,7 +183,6 @@ import { VirtualScrollerModule } from "primeng/virtualscroller";
         TimelineModule,
         ToastModule,
         ToolbarModule,
-
 
         // AccordionModule,
         // AutoCompleteModule,
@@ -298,15 +300,16 @@ import { VirtualScrollerModule } from "primeng/virtualscroller";
         SindicatoService,
 
         AppBreadcrumbService,
-        // CountryService,
         EventService,
         IconService,
         MenuService,
-        // NodeService,
-        // PhotoService,
-        ProductService,
         MessageService,
         ConfirmationService,
+
+        ProductService,  //Se usa en Dashboard gastos
+        // CountryService,
+        // NodeService,
+        // PhotoService,
 
         { provide: LocationStrategy, useClass: HashLocationStrategy },
         {
