8eba028364d7208840e4565de0bc56e3c3fef726
mamjerez <correomam@gmail.com>
2021-12-31 13:55:08 +0100

    Add this Module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AppLoginComponent } from './login/app.login.component';
+import { RegisterComponent } from './register/register.component';
+// import { MainComponent } from './pages/main/main.component';
+
+@NgModule({
+  declarations: [
+    // MainComponent,
+    AppLoginComponent,
+    RegisterComponent
+  ],
+
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule
+  ]
+})
+export class AuthModule { }
