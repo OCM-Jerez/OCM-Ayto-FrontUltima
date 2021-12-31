b4c40599728e09cdd6a3c45b99eab8fb8839d725
mamjerez <correomam@gmail.com>
2021-12-31 13:59:40 +0100

    Add interface IUser

--- src/app/domain/user.ts
+++ src/app/domain/user.ts
@@ -4,4 +4,22 @@ export interface IregisterUser {
     password?: string;
 }
 
-
+export interface IUser {
+    id?: any;
+    createdBy?: string;
+    createdDate?: Date;
+    lastModifiedBy?: string;
+    lastModifiedDate?: Date;
+    login: string;
+    firstName: string;
+    lastName: string;
+    email?: string;
+    activated: boolean;
+    langKey: string;
+    password: string;
+    ImageUrl?: string;
+    ActivationKey?: string;
+    ResetKey?: string;
+    // resetDate: Date;
+    authorities?: string[];
+}
