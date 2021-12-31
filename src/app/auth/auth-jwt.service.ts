6ea48841db4810ee62db2f1b1fcdc417fb6d0784
mamjerez <correomam@gmail.com>
2021-12-31 13:47:20 +0100

    Add this service

---
+++ src/app/auth/auth-jwt.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+
+import { environment } from 'src/environments/environment';
+import { ILogin } from './login/login.interface';
+
+type JwtToken = {
+  id_token: string;
+};
+
+@Injectable({ providedIn: 'root' })
+export class AuthServerProvider {
+  constructor(
+    private http: HttpClient,
+    private $localStorage: LocalStorageService,
+    private $sessionStorage: SessionStorageService
+  ) { }
+
+  login(login: ILogin): Observable<void> {
+    return this.http
+      .post<JwtToken>(environment.host + 'authenticate', login)
+      .pipe(map(response => {
+        // console.log(response);
+        return this.authenticateSuccess(response, true);
+      }));
+  }
+
+  logout(): Observable<void> {
+    return new Observable(observer => {
+      this.$localStorage.clear('authenticationToken');
+      this.$sessionStorage.clear('authenticationToken');
+      observer.complete();
+    });
+  }
+
+  getToken(): string {
+    return this.$localStorage.retrieve('authenticationToken') ||
+      this.$sessionStorage.retrieve('authenticationToken') || '';
+  }
+
+  private authenticateSuccess(response: JwtToken, rememberMe: boolean): void {
+    const jwt = response.id_token;
+    if (rememberMe) {
+      this.$localStorage.store('authenticationToken', jwt);
+    } else {
+      this.$sessionStorage.store('authenticationToken', jwt);
+    }
+  }
+
+}
