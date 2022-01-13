0726ac9c20272642718231e30fd2b3759e7f9f66
mamjerez <correomam@gmail.com>
2022-01-13 17:49:37 +0100

    Minor changes

--- src/app/app.config.component.ts
+++ src/app/app.config.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppMainComponent } from './app.main.component';
 
@@ -139,7 +139,7 @@ export class AppConfigComponent implements OnInit {
 
     scale: number = 14;
 
-    scales: number[] = [12,13,14,15,16];
+    scales: number[] = [12, 13, 14, 15, 16];
 
     themes: any[];
 
@@ -165,65 +165,65 @@ export class AppConfigComponent implements OnInit {
 
     isInputBackgroundChanged: boolean = false;
 
-    constructor(public appMain: AppMainComponent, public app: AppComponent) {}
+    constructor(public appMain: AppMainComponent, public app: AppComponent) { }
 
     ngOnInit() {
         this.themes = [
-            {name: 'indigo', color: '#2f8ee5'},
-            {name: 'pink', color: '#E91E63'},
-            {name: 'purple', color: '#9C27B0'},
-            {name: 'deeppurple', color: '#673AB7'},
-            {name: 'blue', color: '#2196F3'},
-            {name: 'lightblue', color: '#03A9F4'},
-            {name: 'cyan', color: '#00BCD4'},
-            {name: 'teal', color: '#009688'},
-            {name: 'green', color: '#4CAF50'},
-            {name: 'lightgreen', color: '#8BC34A'},
-            {name: 'lime', color: '#CDDC39'},
-            {name: 'yellow', color: '#FFEB3B'},
-            {name: 'amber', color: '#FFC107'},
-            {name: 'orange', color: '#FF9800'},
-            {name: 'deeporange', color: '#FF5722'},
-            {name: 'brown', color: '#795548'},
-            {name: 'bluegrey', color: '#607D8B'}
+            { name: 'indigo', color: '#2f8ee5' },
+            { name: 'pink', color: '#E91E63' },
+            { name: 'purple', color: '#9C27B0' },
+            { name: 'deeppurple', color: '#673AB7' },
+            { name: 'blue', color: '#2196F3' },
+            { name: 'lightblue', color: '#03A9F4' },
+            { name: 'cyan', color: '#00BCD4' },
+            { name: 'teal', color: '#009688' },
+            { name: 'green', color: '#4CAF50' },
+            { name: 'lightgreen', color: '#8BC34A' },
+            { name: 'lime', color: '#CDDC39' },
+            { name: 'yellow', color: '#FFEB3B' },
+            { name: 'amber', color: '#FFC107' },
+            { name: 'orange', color: '#FF9800' },
+            { name: 'deeporange', color: '#FF5722' },
+            { name: 'brown', color: '#795548' },
+            { name: 'bluegrey', color: '#607D8B' }
         ];
 
         this.menuThemes = [
-            {name: 'light', color: '#FDFEFF'},
-            {name: 'dark', color: '#434B54'},
-            {name: 'indigo', color: '#1A237E'},
-            {name: 'bluegrey', color: '#37474F'},
-            {name: 'brown', color: '#4E342E'},
-            {name: 'cyan', color: '#006064'},
-            {name: 'green', color: '#2E7D32'},
-            {name: 'deeppurple', color: '#4527A0'},
-            {name: 'deeporange', color: '#BF360C'},
-            {name: 'pink', color: '#880E4F'},
-            {name: 'purple', color: '#6A1B9A'},
-            {name: 'teal', color: '#00695C'}
+            { name: 'light', color: '#FDFEFF' },
+            { name: 'dark', color: '#434B54' },
+            { name: 'indigo', color: '#1A237E' },
+            { name: 'bluegrey', color: '#37474F' },
+            { name: 'brown', color: '#4E342E' },
+            { name: 'cyan', color: '#006064' },
+            { name: 'green', color: '#2E7D32' },
+            { name: 'deeppurple', color: '#4527A0' },
+            { name: 'deeporange', color: '#BF360C' },
+            { name: 'pink', color: '#880E4F' },
+            { name: 'purple', color: '#6A1B9A' },
+            { name: 'teal', color: '#00695C' }
         ];
 
         this.topbarThemes = [
-            {name: 'lightblue', color: '#2E88FF'},
-            {name: 'dark', color: '#363636'},
-            {name: 'white', color: '#FDFEFF'},
-            {name: 'blue', color: '#1565C0'},
-            {name: 'deeppurple', color: '#4527A0'},
-            {name: 'purple', color: '#6A1B9A'},
-            {name: 'pink', color: '#AD1457'},
-            {name: 'cyan', color: '#0097A7'},
-            {name: 'teal', color: '#00796B'},
-            {name: 'green', color: '#43A047'},
-            {name: 'lightgreen', color: '#689F38'},
-            {name: 'lime', color: '#AFB42B'},
-            {name: 'yellow', color: '#FBC02D'},
-            {name: 'amber', color: '#FFA000'},
-            {name: 'orange', color: '#FB8C00'},
-            {name: 'deeporange', color: '#D84315'},
-            {name: 'brown', color: '#5D4037'},
-            {name: 'grey', color: '#616161'},
-            {name: 'bluegrey', color: '#546E7A'},
-            {name: 'indigo', color: '#3F51B5'}
+            { name: 'lightblue', color: '#2E88FF' },
+            { name: 'dark', color: '#363636' },
+            { name: 'white', color: '#FDFEFF' },
+            { name: 'blue', color: '#1565C0' },
+            { name: 'deeppurple', color: '#4527A0' },
+            { name: 'purple', color: '#6A1B9A' },
+            { name: 'pink', color: '#AD1457' },
+            { name: 'cyan', color: '#0097A7' },
+            { name: 'teal', color: '#00796B' },
+            { name: 'green', color: '#43A047' },
+            { name: 'lightgreen', color: '#689F38' },
+            { name: 'lime', color: '#AFB42B' },
+            { name: 'yellow', color: '#FBC02D' },
+            { name: 'amber', color: '#FFA000' },
+            { name: 'orange', color: '#FB8C00' },
+            { name: 'deeporange', color: '#D84315' },
+            { name: 'brown', color: '#5D4037' },
+            { name: 'grey', color: '#616161' },
+            { name: 'bluegrey', color: '#546E7A' },
+            { name: 'indigo', color: '#3F51B5' }
         ];
 
         this.selectedMenuTheme = this.menuThemes.find(theme => theme.name === this.menuTheme);
@@ -295,7 +295,7 @@ export class AppConfigComponent implements OnInit {
 
         const appLogoLink: HTMLImageElement = document.getElementById('app-logo') as HTMLImageElement;
 
-        if (theme.name == 'white' || theme.name == 'yellow' || theme.name == 'amber'  || theme.name == 'orange' || theme.name == 'lime') {
+        if (theme.name == 'white' || theme.name == 'yellow' || theme.name == 'amber' || theme.name == 'orange' || theme.name == 'lime') {
             appLogoLink.src = 'assets/layout/images/logo-dark.svg';
         }
         else {
