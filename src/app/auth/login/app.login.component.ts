91de8052f1f3aae5fabe6817e5f06317956bee6c
mamjerez <correomam@gmail.com>
2022-01-16 10:15:27 +0100

    Clean code AppLoginComponent

--- src/app/auth/login/app.login.component.ts
+++ src/app/auth/login/app.login.component.ts
@@ -2,23 +2,20 @@ import { Component } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 
-import Swal from 'sweetalert2';
-
-import { LOGIN_VALIDATORS } from "./login.validators"
 import { UserService } from '../../service/user.service';
 
-import { IregisterUser } from "src/app/domain/user";
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { LOGIN_VALIDATORS } from "./login.validators"
 
+import { IregisterUser } from "src/app/domain/user";
+
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './app.login.component.html',
   styleUrls: ['./app.login.component.scss'],
-
 })
+
 export class AppLoginComponent {
-  miError: string = '';
   formGroup: FormGroup;
   private _user: IregisterUser;
 
@@ -58,18 +55,9 @@ export class AppLoginComponent {
     }
 
     const res = this._userService.login(this._user)
-      // .pipe(
-      //   catchError(err => {
-      //     console.log("error", err);
-      //     return throwError(err);
-      //   })
-      // )
       .subscribe(
         async response => {
           if (response) this._router.navigate(['/favorites/dashboardanalytics']);
-          //  else {
-          //   Swal.fire('', `Usuario o password erroneo`, 'error');
-          // }
         },
         error => {
           // Si no existe el Usuario.
