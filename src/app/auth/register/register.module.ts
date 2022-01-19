a8028e49d832bff6c195182675c79a00c5062ead
mamjerez <correomam@gmail.com>
2022-01-19 10:25:11 +0100

    Clean code

--- src/app/auth/register/register.module.ts
+++ src/app/auth/register/register.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 import { SharedFormModule } from 'src/common/shared/shared-form.module';
 
 import { RegisterComponent } from './register.component';
-
-import { RouterModule, Routes } from '@angular/router';
 
 //auth/register/
 export const routes: Routes = [{ path: "", component: RegisterComponent }]
 
 @NgModule({
     declarations: [RegisterComponent],
-    imports: [RouterModule.forChild(routes), SharedFormModule]
+    imports: [
+        RouterModule.forChild(routes),
+        SharedFormModule
+    ]
 })
 export class RegisterModule { }
--- src/common/shared/shared-form.module.ts
+++ src/common/shared/shared-form.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 
-
 @NgModule({
-    exports: [ReactiveFormsModule, FormsModule, InputTextModule, InputTextareaModule, ButtonModule],
+    exports: [
+        ReactiveFormsModule,
+        FormsModule,
+        InputTextModule,
+        InputTextareaModule,
+        ButtonModule
+    ],
 })
 export class SharedFormModule { }
