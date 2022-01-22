61e430a8d62b990adc97f1715b5753ec374c115d
mamjerez <correomam@gmail.com>
2022-01-22 14:31:06 +0100

    Use index in service & domain

--- src/app/pages/home/seccion-censal/seccion-censal.component.ts
+++ src/app/pages/home/seccion-censal/seccion-censal.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
-import { SeccionCensal } from "../../../domain/seccion-censal";
-import { SeccionCensalService } from "../../../service/seccion-censal.service";
-import { ConfirmationService } from "primeng/api";
-import { MessageService } from "primeng/api";
-import { AppBreadcrumbService } from "../../../layout/breadcrumb/app.breadcrumb.service";
 import { Observable } from "rxjs";
 
+import { SeccionCensalService } from "../../../service";
+import { AppBreadcrumbService } from "../../../layout/breadcrumb/app.breadcrumb.service";
+
+import { SeccionCensal } from "../../../domain";
 @Component({
   selector: 'app-organoContratacion',
   templateUrl: './seccion-censal.component.html',
   styleUrls: ['./seccion-censal.component.scss'],
-  providers: [MessageService, ConfirmationService]
 })
+
 export class SeccionCensalComponent implements OnInit {
   _dialog: boolean;
   _obs$: Observable<SeccionCensal[]>;
-
   _obj: SeccionCensal;
   _selected: SeccionCensal[];
   submitted: boolean;
@@ -25,8 +23,6 @@ export class SeccionCensalComponent implements OnInit {
 
   constructor(
     private organoContratacionService: SeccionCensalService,
-    private messageService: MessageService,
-    private confirmationService: ConfirmationService,
     private breadcrumbService: AppBreadcrumbService,
     private router: Router
   ) {
@@ -44,7 +40,6 @@ export class SeccionCensalComponent implements OnInit {
       { field: "Descripcion", header: "Descripcion" },
       { field: "Observaciones", header: "Observaciones" },
       { field: "WEBOCM", header: "WEBOCM" },
-      // { field: 'inventoryStatus', header: 'Status' }
     ];
   }
 
