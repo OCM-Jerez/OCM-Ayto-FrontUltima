30550b638129be5a6a09a67e769b76496c891158
mamjerez <correomam@gmail.com>
2022-01-22 13:08:20 +0100

    Use index in service & domain

--- src/app/pages/home/distritos/distritos.component.ts
+++ src/app/pages/home/distritos/distritos.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
-import { Distrito } from "../../../domain/distrito";
-import { DistritoService } from "../../../service/distrito.service";
-import { ConfirmationService } from "primeng/api";
-import { MessageService } from "primeng/api";
-import { AppBreadcrumbService } from "../../../layout/breadcrumb/app.breadcrumb.service";
 import { Observable } from "rxjs";
+
+import { DistritoService } from "../../../service";
+import { AppBreadcrumbService } from "../../../layout/breadcrumb/app.breadcrumb.service";
+
+import { Distrito } from "../../../domain";
 
 @Component({
   selector: 'app-distrito',
   templateUrl: './distritos.component.html',
   styleUrls: ['./distritos.component.scss'],
-  providers: [MessageService, ConfirmationService]
 })
+
 export class DistritosComponent implements OnInit {
   _dialog: boolean;
   _obs$: Observable<Distrito[]>;
-
   _obj: Distrito;
   _selected: Distrito[];
   submitted: boolean;
@@ -25,8 +24,6 @@ export class DistritosComponent implements OnInit {
 
   constructor(
     private distritoService: DistritoService,
-    private messageService: MessageService,
-    private confirmationService: ConfirmationService,
     private breadcrumbService: AppBreadcrumbService,
     private router: Router
   ) {
@@ -44,7 +41,6 @@ export class DistritosComponent implements OnInit {
       { field: "Descripcion", header: "Descripcion" },
       { field: "Observaciones", header: "Observaciones" },
       { field: "WEBOCM", header: "WEBOCM" },
-      // { field: 'inventoryStatus', header: 'Status' }
     ];
   }
 
