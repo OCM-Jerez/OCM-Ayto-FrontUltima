1844ab9f965f54dff0e9f447a271a56f441a736a
mamjerez <correomam@gmail.com>
2022-01-22 14:32:28 +0100

    Use index in service & domain

--- src/app/pages/home/sindicatos/sindicatos.component.ts
+++ src/app/pages/home/sindicatos/sindicatos.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
-import { Sindicato } from "../../../domain/sindicato";
-import { SindicatoService } from "../../../service/sindicato.service";
-import { ConfirmationService } from "primeng/api";
-import { MessageService } from "primeng/api";
-import { AppBreadcrumbService } from "../../../layout/breadcrumb/app.breadcrumb.service";
 import { Observable } from "rxjs";
 
+import { SindicatoService } from "../../../service";
+import { AppBreadcrumbService } from "../../../layout/breadcrumb/app.breadcrumb.service";
+
+import { Sindicato } from "../../../domain";
 @Component({
   selector: 'app-sindicato',
   templateUrl: './sindicatos.component.html',
   styleUrls: ['./sindicatos.component.scss'],
-  providers: [MessageService, ConfirmationService]
 })
+
 export class SindicatosComponent implements OnInit {
   _dialog: boolean;
   _obs$: Observable<Sindicato[]>;
-
   _obj: Sindicato;
   _selected: Sindicato[];
   submitted: boolean;
@@ -25,8 +23,6 @@ export class SindicatosComponent implements OnInit {
 
   constructor(
     private sindicatoService: SindicatoService,
-    private messageService: MessageService,
-    private confirmationService: ConfirmationService,
     private breadcrumbService: AppBreadcrumbService,
     private router: Router
   ) {
@@ -44,7 +40,6 @@ export class SindicatosComponent implements OnInit {
       { field: "Descripcion", header: "Descripcion" },
       { field: "Observaciones", header: "Observaciones" },
       { field: "WEBOCM", header: "WEBOCM" },
-      // { field: 'inventoryStatus', header: 'Status' }
     ];
   }
 
