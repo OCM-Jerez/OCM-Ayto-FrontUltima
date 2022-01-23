b5fa8a227dd163784c683c5e95088d1f443d70d6
mamjerez <correomam@gmail.com>
2022-01-23 21:46:25 +0100

    Change button

--- src/app/pages/404notFound/app.notfound.component.ts
+++ src/app/pages/404notFound/app.notfound.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notfound',
   templateUrl: './app.notfound.component.html',
+  styleUrls: ['./app.notfound.component.scss'],
+
 })
 export class AppNotfoundComponent {
 
