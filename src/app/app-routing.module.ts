e70a85c79d95d331cbf5a96ea7950168233e2208
mamjerez <correomam@gmail.com>
2022-01-23 11:12:11 +0100

    Clean code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,14 @@ import { NgModule } from "@angular/core";
 import { AppErrorComponent } from "./pages/error/app.error.component";
 import { AppLoginComponent } from "./auth/login/app.login.component";
 import { AppNotfoundComponent } from "./pages/404notFound/app.notfound.component";
-import { HomeGuard } from "../common/guards/home.guard";
 
+import { HomeGuard } from "../common/guards/home.guard";
 
 @NgModule({
     imports: [
         RouterModule.forRoot(
             [
                 {
-
                     path: "",
                     canActivate: [HomeGuard],
                     loadChildren: () => import("./pages/home/app-main.module").then(m => m.AppMainModule)
