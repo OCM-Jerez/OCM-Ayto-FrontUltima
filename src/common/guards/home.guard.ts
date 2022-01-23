9ccd88f351a9202cb457845033e9762af66299ab
mamjerez <correomam@gmail.com>
2022-01-23 11:13:20 +0100

    Clean code

--- src/common/guards/home.guard.ts
+++ src/common/guards/home.guard.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+
 import { JwtValidService } from '../services/token-valid.service';
 
 @Injectable({ providedIn: 'root' })
 export class HomeGuard implements CanActivate {
-    constructor(private _jwtValidService: JwtValidService, private _router: Router) { }
+    constructor(
+        private _jwtValidService: JwtValidService,
+        private _router: Router
+    ) { }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
 
         if (this._jwtValidService.isExpired()) {
